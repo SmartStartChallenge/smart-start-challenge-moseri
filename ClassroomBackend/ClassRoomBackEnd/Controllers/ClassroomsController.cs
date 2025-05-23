451ea50683640f8ff067053a21a8fd49835efa8a
Mangope Moseri <moserimad@yahoo.com>
2025-05-23 12:06:28 +0200

    Changes Made

---
+++ ClassroomBackend/ClassRoomBackEnd/Controllers/ClassroomsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClassRoomBackEnd.Controllers
+{
+    public class ClassroomsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ClassroomBackend/ClassRoomBackEnd/Models/Classroom.cs
+++ ClassroomBackend/ClassRoomBackEnd/Models/Classroom.cs
@@ -1,9 +1,33 @@
-﻿namespace ClassRoomBackEnd.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ClassRoomBackEnd.Models
 {
     public class Classroom
     {
+        [Key]
         public int Id { get; set; }
 
-        // To be added
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Capacity must be at least 1.")]
+        public int Capacity { get; set; }
+
+        [Required]
+        public string TeacherId { get; set; }
+
+        [ForeignKey("TeacherId")]
+        public IdentityUser Teacher { get; set; }
+
+        public ICollection<ApplicationUser> Learners { get; set; } = new List<ApplicationUser>();
+
+
+
     }
 }
